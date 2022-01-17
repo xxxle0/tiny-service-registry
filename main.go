fad424b3b5f09d882bf8c461eeeb89d627414b3a
duytq <duybkit13@gmail.com>
2022-01-17 23:50:38 +0700

    feat: Define Service Record struct

--- main.go
+++ main.go
@@ -12,7 +12,16 @@ func (this *MainController) Get() {
 	this.Ctx.WriteString("hello world!")
 }
 
+type ServiceRecord struct {
+	ServiceName     string `json:"service_name"`
+	ServiceIP       string `json:"service_ip"`
+	ServicePort     int    `json:"service_port"`
+	HealthCheckPath string `json:"health_check_path"`
+	HttpScheme      string `json:"http_scheme"`
+}
+
 func main() {
-	web.Router("/", &MainController{})
+	web.Router("/api/v1/services", &MainController{})
+	web.Router("/api/v1/services", &MainController{})
 	web.Run()
 }
